ff62af8d2d4d3ab8b9255578425748d18f96d16a
Thomas Nattestad <thomas.nattestad@gmail.com>
2019-08-02 16:09:52 +0200

    added some controls

--- fractalClock/fractalClockProcessing.js
+++ fractalClock/fractalClockProcessing.js
@@ -1,12 +1,31 @@
-let clockWidth;
-let clockHeight;
-let clockHandLength;
-let timeSpeed = 1;
-let scalingFactor = .7;
 
-let currentTime = 0;
-let currentTime1 = 8;
-let currentTime2 = 52;
+let ClockControls = function () {
+    this.clockWidth = 0
+    this.clockHeight = 0
+    this.clockHandLength = 0
+    this.timeSpeed = 1
+    this.scalingFactor = .7
+    this.maxDepth = 10
+    this.minLength = 2.0
+    this.currentTime1 = 0;
+    this.currentTime2 = 0;
+};
+
+let clockControls;
+function setupControls() {
+    clockControls = new ClockControls();
+    var gui = new dat.GUI();
+    gui.add(clockControls, 'clockHandLength', 0, 500).listen();
+    gui.add(clockControls, 'scalingFactor', 0, 1).listen();
+    
+    gui.add(clockControls, 'currentTime1', 0, 60).listen();
+    gui.add(clockControls, 'currentTime2', 0, 60).listen();
+
+    gui.add(clockControls, 'maxDepth', 1, 20).listen();
+    gui.add(clockControls, 'minLength', 0, 5.0).listen();
+
+    gui.add(clockControls, 'timeSpeed', 0, 100).listen();
+}
 
 let theCanvas;
 let context;
@@ -14,42 +33,44 @@ let context;
 let previousTime = 0;
 
 function setup() {
+    setupControls();
 
-    clockWidth = document.getElementsByTagName("body")[0].clientWidth - 20;
-    clockHeight = document.getElementsByTagName("body")[0].clientHeight - 20;
+    clockControls.clockWidth = document.getElementsByTagName("body")[0].clientWidth - 20;
+    clockControls.clockHeight = document.getElementsByTagName("body")[0].clientHeight - 20;
 
-    if(clockWidth > clockHeight){
-        clockHandLength = clockHeight;
-    }else{
-        clockHandLength = clockWidth;
+    if (clockControls.clockWidth > clockControls.clockHeight) {
+        clockControls.clockHandLength = clockControls.clockHeight;
+    } else {
+        clockControls.clockHandLength = clockControls.clockWidth;
     }
-    clockHandLength = clockHandLength / 5;
+    clockControls.clockHandLength = clockControls.clockHandLength / 5;
 
-    createCanvas(clockWidth, clockHeight);
+    createCanvas(clockControls.clockWidth, clockControls.clockHeight);
 
     strokeWeight(1);
 }
 
 function draw() {
     clear();
-    currentTime += deltaTime;
     strokeWeight(1);
 
     // print(time);
-    currentTime1 = (currentTime / 1000) / 60;
-    currentTime2 = currentTime / 1000;
+    clockControls.currentTime1 = clockControls.currentTime1 + ((deltaTime * clockControls.timeSpeed) / 1000 / 60);
+    clockControls.currentTime2 = clockControls.currentTime2 + ((deltaTime * clockControls.timeSpeed) / 1000);
 
+    if(clockControls.currentTime1 >= 60) clockControls.currentTime1 = 0;
+    if(clockControls.currentTime2 >= 60) clockControls.currentTime2 = 0;
 
     // rainbowStyle(currentTime);
 
-    drawHands(clockWidth / 2, clockHeight / 2, (clockHandLength) * scalingFactor, currentTime1, currentTime2, 0);
+    drawHands(clockControls.clockWidth / 2, clockControls.clockHeight / 2, (clockControls.clockHandLength) * clockControls.scalingFactor, clockControls.currentTime1, clockControls.currentTime2, 0);
 
     //framerate 
     console.log("Frame Rate: ", 1 / (deltaTime / 1000))
 }
 
 function drawHands(x, y, length, time1, time2, depth) {
-    if (depth > 10 || length < 2) {
+    if (depth >= clockControls.maxDepth || length < clockControls.minLength) {
         // console.log("Hit dat limit!")
         return
     }
@@ -63,8 +84,8 @@ function drawHands(x, y, length, time1, time2, depth) {
     // circle(newX1, newY1, 5);
     // circle(newX2, newY2, 5);
 
-    drawHands(newX1, newY1, length * scalingFactor, time1 + currentTime1, time1 + currentTime2, depth + 1);
-    drawHands(newX2, newY2, length * scalingFactor, time2 + currentTime1, time2 + currentTime2, depth + 1);
+    drawHands(newX1, newY1, length * clockControls.scalingFactor, time1 + clockControls.currentTime1, time1 + clockControls.currentTime2, depth + 1);
+    drawHands(newX2, newY2, length * clockControls.scalingFactor, time2 + clockControls.currentTime1, time2 + clockControls.currentTime2, depth + 1);
 }
 
 function drawLineWithTime(x, y, length, time) {
